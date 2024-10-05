0f61856da818a85183b4bd1686aa71d6683e17b9
M Asim <asimsaim549@gmail.com>
2024-10-04 22:54:44 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7596784753:AAH0iD9GBmTncdVQhcjPBlt-D7rQfuzKp2g";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
